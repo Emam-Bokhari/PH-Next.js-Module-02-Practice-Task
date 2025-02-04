addb8dc80c8f0bc38a69a04b80a073023083d536
Moshfiqur Rahman <studentemam@gmail.com>
2025-02-04 22:32:58 +0600

    feat: add blogs page and implement server side rendering

--- src/app/(commonLayout)/blogs/page.js
+++ src/app/(commonLayout)/blogs/page.js
@@ -1,3 +1,15 @@
+import BlogCard from "@/components/posts/BlogCard";
+
 export default async function BlogsPage() {
-  return <div></div>;
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    cache: "no-store",
+  });
+  const posts = await response.json();
+  return (
+    <div className="grid grid-cols-3 gap-10 my-10">
+      {posts?.slice(0, 6)?.map((blog) => (
+        <BlogCard key={blog.id} blog={blog} />
+      ))}
+    </div>
+  );
 }
